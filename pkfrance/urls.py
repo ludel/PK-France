850ec3a166f7d7afb6452a5106146db93be3b68c
ludel <ludovic.delsol@ynov.com>
2019-09-07 22:36:10 +0200

    Pep8 fix

--- pkfrance/urls.py
+++ pkfrance/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='pkfrance/index.html'), name='index'),
 
     path('blog/', include('blog.urls')),
     path('dj-admin/', admin.site.urls),
+]
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
